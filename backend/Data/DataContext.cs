924e1fa4992c82e818780120a39fcac70f76fc91
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2025-10-13 16:08:00 -0300

    fea: adiciona o arquivo de conexão com o banco

---
+++ backend/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Superheroes.Api.Entities;
+
+namespace Superheroes.Api.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    
+    public DbSet<Herois> Herois { get; set; }
+    public DbSet<Superpoderes> Superpoderes { get; set; }
+}
