ec33b665a17855804218455bde2e522ca4f1eab9
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2025-10-13 15:45:03 -0300

    feat(Entities): adiciona as entidades Heroi e Superpoderes

---
+++ backend/Entities/Herois.cs
@@ -0,0 +1,12 @@
+﻿namespace Superheroes.Api;
+
+public class Herois
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string NomeHeroi { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public float Altura { get; set; }
+    public float Peso { get; set; }
+    public ICollection<Superpoderes> Superpoderes { get; set; }
+}
---
+++ backend/Entities/Superpoderes.cs
@@ -0,0 +1,9 @@
+﻿namespace Superheroes.Api;
+
+public class Superpoderes
+{
+    public int Id { get; set; }
+    public string Superpoder { get; set; }
+    public string Descricao { get; set; }
+    public ICollection<Herois> Herois { get; set; }
+}
