fee0286387e7a6ad84736c193a739029001d1c63
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2025-10-14 10:11:10 -0300

    feat(DTO): adiciona o campo Id no HeroiDTOCriacao

--- backend/DTOs/HeroiDTOCriacao.cs
+++ backend/DTOs/HeroiDTOCriacao.cs
@@ -7,6 +7,7 @@ namespace backend.DTOs
 {
     public class HeroiDTOCriacao
     {
+        public int Id { get; set; }
         public string Nome { get; set; }
         public string NomeHeroi { get; set; }
         public DateTime DataNascimento { get; set; }
