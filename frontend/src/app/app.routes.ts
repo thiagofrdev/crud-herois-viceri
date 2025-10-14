3f96fbd88f66b87503d363914eb29a258e5d03ea
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2025-10-14 16:02:54 -0300

    feat: configura as rotas entre as páginas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HeroListComponent } from './pages/hero-list/hero-list.component'
+import { HeroFormComponent } from './pages/hero-form/hero-form.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //Rota 1: Redirecionamento da raiz
+    { path: '', redirectTo: '/heroes', pathMatch: 'full' },
+
+    //Rota 2: Lista de Heróis
+    { path: 'heroes', component: HeroListComponent },
+
+    //Rota 3: Formulário de Criação
+    { path: 'heroes/new', component: HeroFormComponent },
+
+    //Rota 4: Formulário d Edição
+    { path: 'heroes/edit/:id', component: HeroFormComponent }
+];
